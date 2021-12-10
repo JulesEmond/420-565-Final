df1d46d27309788efabae119beca917b715233d5
Jules Emond <julesemond@outlook.com>
2021-12-10 16:43:17 -0500

    Creation du controller

--- src/main/java/com/lacouf/al420565final/controller/ClientController.java
+++ src/main/java/com/lacouf/al420565final/controller/ClientController.java
@@ -1,11 +1,35 @@
 package com.lacouf.al420565final.controller;
 
-
+import com.lacouf.al420565final.ClientFactory;
+import com.lacouf.al420565final.model.Client;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class ClientController {
 
+    @GetMapping("/all")
+    public List<Client> getAll(){
+        return ClientFactory.getClients();
+    }
+
+    @GetMapping("/hommes")
+    public List<Client> getHommes(){
+        return ClientFactory.getClients()
+                .stream()
+                .filter(c -> c.getGender().equals("M"))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/ontariens")
+    public List<Client> getOntariens(){
+        return ClientFactory.getClients()
+                .stream()
+                .filter(c -> c.getProvince().equals("ON"))
+                .collect(Collectors.toList());
+    }
 }
