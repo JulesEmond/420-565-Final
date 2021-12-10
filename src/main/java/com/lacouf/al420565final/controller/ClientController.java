cf31bdf4ca9650f1aa84073677f0b5ebcabbd407
Jules Emond <julesemond@outlook.com>
2021-12-10 16:38:12 -0500

    Initial commit

---
+++ src/main/java/com/lacouf/al420565final/controller/ClientController.java
@@ -0,0 +1,11 @@
+package com.lacouf.al420565final.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class ClientController {
+
+}
